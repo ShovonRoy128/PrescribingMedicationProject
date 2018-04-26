7d3ea68f4984f03a1dc9a47235d65974fabe7fae
a-pramanik37 <bappycseju37@gmail.com>
2018-04-27 00:46:36 +0600

    main program added

---
+++ mainProgram.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void current medication()
+{
+
+}
+
+void new medication()
+{
+
+}
+
+void formulary()
+{
+
+}
+
+int main()
+{
+    ///UI will be here
+}
