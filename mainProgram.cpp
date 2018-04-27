417dc9831a8367c7d76351f8b1bc3a2369f5e7f6
a-pramanik37 <bappycseju37@gmail.com>
2018-04-27 11:27:35 +0600

    updated current_medication function

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,19 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+std::fstream fs;
 void current_medication()
 {
     cout<<"Check the previous doses here: "<<endl<<endl;
 
   string ss;
-  fstream myfile ("doses.txt");
-  if (myfile.is_open()){
-    while(getline(myfile,ss)){
+  fs.open ("doses.txt", std::fstream::in | std::fstream::out);
+
+
+  if (fs.is_open()){
+    while(getline(fs,ss)){
       cout<<ss<<endl;
     }
-    myfile.close();
+    fs.close();
   }
+  int n;
+  cout<<"If you want to add new doses plz enter how many doses else press 0 : ";
+  cin>>n;
 
+  if(n==0)return;
+  int cnt = 0;
+
+
+  fs.open ("doses.txt", std::fstream::in | std::fstream::out | std::fstream::app);
+
+
+    cin.ignore();
+  while(cnt<n){
+    cout<<"Enter new dose "<<++cnt<<" :"<<endl;
+    getline(cin, ss);
+    fs<<ss;
+    fs<<"\n";
+  }
+  cout<<"Your new doses have been added! Thank you. :)\n";
+    fs.close();
 }
 
 void new_medication()
