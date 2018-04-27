1a44fee4afc06d4b11d68c52912bdcf787f01ef7
HumayraFerdousTasnu <37269881+HumayraFerdousTasnu@users.noreply.github.com>
2018-04-27 09:50:17 +0600

    Update

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,20 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+class patient
+{
+    private:
+    int age,Serialno;
+    string typeofdisease,name;
+    string cmedication,nmedication,formulary;
+    public:
+      void illpatient()
+    {
+        cout<<"Enter serial no: ";
+        cin>>Serialno;
+        cout<<"Enter name: ";
+        cin>>name;
+        cout<<"Enter age: ";
+        cin>>age;
+    }
 
 void current_medication()
 {
-
+ cout<<"Enter type of disease: ";
+        cin>>typeofdisease;
+        cout<<"Enter current medication: ";
+        cin>>cmedication;
 }
 
 void new_medication()
 {
-
+cout<<"Enter type of disease: ";
+        cin>>typeofdisease;
+        cout<<"Enter New medication: ";
+        cin>>nmedication;
 }
 
 void formulary()
 {
-
+cout<<"Enter formulary: ";
+        cin>>formulary;
 }
+};
 
 int main()
 {
