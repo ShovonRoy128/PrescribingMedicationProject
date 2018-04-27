f769538382b5ea98b0a91570cdb3edea389bd20e
Shovon Roy <37194504+ShovonRoy128@users.noreply.github.com>
2018-04-27 12:37:05 +0600

    new_medication_shovon1

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -8,7 +8,23 @@ void current_medication()
 
 void new_medication()
 {
-
+    cout<<"Enter new patient's name : ";
+    cin>>patientName;
+    cout<<"Enter new patient's age : ";
+    cin>>patientAge;
+    do
+    {
+        cout<<"Give medicine?? (y/n) : ";
+        cin>>yesOrNo;
+        cout<<"Enter medicines : ";
+        cin>>medicineName;//here will be shown a list of all medicines based on this name as prefix
+        cout<<"Enter dose/day : ";
+        cin>>dosePerDay;
+    }
+    while(yesOrNo!="n");
+    cout<<"Enter doctor name : ";//doctor who have prescribed
+    cin>>doctorName;
+    //now store that document in the database or file
 }
 
 void formulary()
